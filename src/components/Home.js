9ac9d2337530f4443c6f22e1065496177bcfc181
stephstout <stephsstout@gmail.com>
2021-07-15 22:28:23 -0600

    structure and some styling

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>hi i'm steph!</h1>
+            <p>I’m a detail-oriented, result-driven software developer with a background in design. I have an eye for aesthetics having worked on the front end with other software developers for over 10 years. My wide range of skills have driven conclusive results in the areas of sales and customer engagement.</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/ProjectGallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProjectGallery = () => {
+    return (
+        <div className="project-gallery">
+           <h2>things i've made</h2>
+           <img src="/Users/stephaniestout/projects/portfolio/public/images/imageboard.png" alt=" image board project" />
+           <img src="public/images/lifeonmars.png" alt="life on mars" />
+           <img src="/Users/stephaniestout/projects/portfolio/public/images/blog.png" alt="blog" />
+        </div>
+    );
+};
+
+export default ProjectGallery;
